2bb9df9c75bed39eda4e26bd2de1526f00fd43d6
Garrett Ruths <ruthsgarrett@gmail.com>
2021-12-28 10:46:57 -0500

    I hate json

--- SteamCMDHelper.py
+++ SteamCMDHelper.py
@@ -4,18 +4,61 @@
 import sys
 import os
 import argparse
+from os.path import exists
+
 import json
 # import subprocess
 from zipfile import ZipFile
 
 import urllib.request
 
-class Settings:
+class Settings_JSON:
     install_dir = ""
-    installed_games =	{
-        "example_game": 123 #Once im done working on this i will command this line out
-    }
+    installed_games = {}
 
+class game_info:
+    name = "" #Semi redundant becuase the name will be used to place it in the dictionary
+    steam_id = 0
+    install_dir = ""
+    #Other things ill need on the per game basis?
+
+
+class Settings:
+    valid = False #Used for error checking in the JSON load process
+    json = Settings_JSON()
+
+    def SaveJSON(self):
+        d_print(level_all, "Saving UserSettings.json")
+    
+    def GenerateJSON():
+        settings = Settings()
+        d_print(level_all, "Generating new UserSettings.json")
+        settings.valid = True #This doesnt matter since im not returning it but this makes me feel better
+        settings.json.install_dir = "./SteamCMD"
+        example_game = game_info()
+        example_game.name = "example_game"
+        example_game.steam_id = "999999999999"
+        example_game.install_dir = "./install_dir"
+        settings.json.installed_games["example_game"] = example_game
+        settings.json.installed_games["poo"] = "poo"
+        #print(settings.json.installed_games)
+        x = json.dumps(settings.json, default=lambda o: o.__dict__, indent=4)
+        #y = json.dumps(settings.json) 
+        print(x)
+        return settings
+
+    def LoadJSON():
+        #TODO - make this error resistant, like if the json does exist or there arnt the correct pieces of information in the file
+        userSettings = Settings()
+        try:
+            d_print(level_verbose, "Loading UserSettings JSON")
+            J = json.load(open('UserSettings.json'))
+            userSettings.valid = True
+            userSettings.json.install_dir = J["install_dir"]
+            userSettings.json.installed_games = J["games"]
+        except BaseException as err:
+            d_print(level_all, "Error Loading UserSettings.json")
+        return userSettings
 
 
 #Would put this in an enum but I cant think of a good reason to, its only 3 levels
@@ -52,7 +95,7 @@ def main():
         log_level = 2
         d_print(level_debug, "Debug Logging Enabled")
 
-    if args.dryrun:
+    if args.dry_run:
         d_print(level_all, "Dry Run Enabled - No files will be downloaded and no commands will be passed to the SteamCommandLine")
         dryrun = True
 
@@ -65,34 +108,50 @@ def main():
     d_print(level_all, "Welcome to the SeamCMDHelper")
     d_print(level_all, "\n---------------------------\n")
 
+    #Dear python, support switch statements
+    if(exists("UserSettings.json")):
+        userSettings = Settings.LoadJSON()
+    else:
+        d_print(level_all, "No UserSettings.json file found, would you like to generate a default file?")
+        if(input("\tY - Generates File\n\tAnything Else Exits\n") == "Y"):
+            Settings.GenerateJSON()
+        else:
+            return 0
+        
+
+    # usr_input = input("Selection an action\n")
+    # if(usr_input == "1"):
+    #     print("You typed one")
 
     #MAIN USER INPUT LOOP
 
+    #print(userSettings.install_dir)
 
+    #downloadCMD(userSettings.json.install_dir)
 
-
-    
-    #download
-    downloadCMD("SteamCMD")
-
-    #print(sys.platform)
     return 0
 
-def LoadJSON():
-    import json #why do i have to do this here?
-    d_print(level_debug, "Loading UserSettings JSON")
-    json = json.loads(open("UserSettings.json"))
-    userSettings = Settings()
-    return 
+# def LoadJSON():
+#     #TODO - make this error resistant, like if the json does exist or there arnt the correct pieces of information in the file
+#     try:
+#         d_print(level_verbose, "Loading UserSettings JSON")
+#         J = json.load(open('UserSettings.json'))
+#         userSettings = Settings()
+#         userSettings.valid = True
+#         userSettings.install_dir = J["install_dir"]
+#         userSettings.installed_games = J["games"]
+#         return userSettings
+#     except BaseException as err:
+#         d_print(level_all, "Error Loading UserSettings.json")
 
-def SaveJSON():
-    d_print(level_debug, "Saving UserSettings JSON")
+# def SaveJSON(uSettings):
+#     d_print(level_verbose, "Saving UserSettings JSON")
 
 
 def downloadCMD(install_location):
     #always make the folder, if it already exists I doubt itll fail
     if not os.path.exists(install_location):
-        os.mkdir(install_location)
+        os.mkdir(install_location) #Cant make multiple folder depths
     install_location = install_location + "/" #hope this doesnt screw up windows
 
     d_print(level_all, "Downloading SteamCMD into folder: " + install_location)
